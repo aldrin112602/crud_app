c3c9b7109986ab3766ebb3325559a4ec6d62e499
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-05-25 13:12:20 +0800

    Updated final na

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,22 +4,20 @@
 
 @section('content')
 <div class="p-5">
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+    
     <form method="POST" action="{{ route('login.submit') }}" class="mx-auto col-lg-6 col bg-light border rounded shadow" style="overflow: hidden;">
         @csrf
         <h4 class="bg-primary p-4 text-white">Login</h4>
         <div class="p-4">
-            @error('error')
-                <div class="alert alert-danger p-2 mt-2">{{ $message }}</div>
-            @enderror
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             <div class="mt-2">
                 <label for="email" class="form-label">Email address</label>
                 <input name="email" type="email" class="form-control" id="email" value="{{ old('email') }}" required>
